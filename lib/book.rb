e0e0e4671d0f2e2d958b3bb8e14d890393531b07
Chris Wade <swade781977@yahoo.com>
2020-01-29 03:16:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,8 @@
 class Book
   def initialize(title)
     @title = title
-  def attr_accessor :author, :page_count, :genre
-  def title
-    @title
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
   def turn_page
     puts "Flipping the page...wow, you read fast!"
 
