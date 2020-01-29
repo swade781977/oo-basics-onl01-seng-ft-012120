8b83e78c9c046350111a69577ac7c1ac5f8356a6
Chris Wade <swade781977@yahoo.com>
2020-01-29 03:16:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,9 +2,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
-  def brand
-    @brand
-  end
+  attr_reader :brand
   attr_accessor :color, :size, :material, :condition
   def cobble
     puts "Your shoe is as good as new!"
