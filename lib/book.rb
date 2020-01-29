04c9ea4f69df06a6c25ba6c160b42d07b638e2c9
Chris Wade <swade781977@yahoo.com>
2020-01-29 02:46:22 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,17 @@
 class Book
-  def initialize()
-  end
+  def initialize(title, author, page_count, genre)
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+   end
+   def turn_page(page_count)
+     current_page = 0
+     if current_page < page_count
+       current_page += 1
+     else
+       puts "Book is finished! No more pages"
+     end
+   end
 end
 
